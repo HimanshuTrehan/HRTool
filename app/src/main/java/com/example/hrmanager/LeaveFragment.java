2dc018518bb6a49635e4e0d19b962d9009c2362c
Himanshu <himanshutrehan2000@gmail.com>
2021-09-09 12:55:33 +0530

    Basic done

--- app/src/main/java/com/example/hrmanager/LeaveFragment.java
+++ app/src/main/java/com/example/hrmanager/LeaveFragment.java
@@ -1,18 +1,22 @@
 package com.example.hrmanager;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class LeaveFragment extends Fragment {
 
     View v;
-    private static final String[] paths = {"item 1", "item 2", "item 3"};
+    private static final String[] paths = {"Sick Leave", "Compensatory Leave", "Earned Leave"};
+    private static final String[] paths1 = {"Half Day Leave", "Full Day Leave"};
 
     public LeaveFragment() {
         // Required empty public constructor
@@ -37,6 +41,39 @@ public class LeaveFragment extends Fragment {
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                ((TextView) adapterView.getChildAt(0)).setTextColor(Color.BLACK);
+                ((TextView) adapterView.getChildAt(0)).setTextSize(16);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+               Spinner spinner1 = (Spinner)v.findViewById(R.id.spinner2);
+        ArrayAdapter<String>adapters = new ArrayAdapter<String>(getContext(),
+                android.R.layout.simple_spinner_item,paths1);
+
+        adapters.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapters);
+
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                ((TextView) adapterView.getChildAt(0)).setTextColor(Color.BLACK);
+                ((TextView) adapterView.getChildAt(0)).setTextSize(16);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
     }
 }
